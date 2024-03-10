8a7d045541aa2c89174a9f464c5617fff50374c7
FatimaOmair <fatimaemer2002@gmail.com>
2024-03-10 22:24:14 +0200

    import super route

--- src/modules/index.router.js
+++ src/modules/index.router.js
@@ -2,4 +2,5 @@ import authRouter from "./auth/auth.router.js";
 import adminRouter from './admin/admin.router.js'
 import headRouter from './headOfdepartment/head.router.js'
 import studentRouter from './student/students.router.js'
-export { authRouter, adminRouter,headRouter,studentRouter }
+import supervisorRouter from './supervisor/super.router.js'
+export { authRouter, adminRouter,headRouter,studentRouter,supervisorRouter }
