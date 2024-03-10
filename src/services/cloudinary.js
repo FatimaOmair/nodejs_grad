25eb2221778987c31ae51c30759a2246b4b87291
FatimaOmair <fatimaemer2002@gmail.com>
2024-03-10 09:14:08 +0200

    add my account for cloudinary

--- src/services/cloudinary.js
+++ src/services/cloudinary.js
@@ -2,8 +2,8 @@ import cloudinary from "cloudinary";
 
 // Configuration
 cloudinary.v2.config({
-  cloud_name: "dfypdmpja",
-  api_key: "338292164954233",
-  api_secret: "eXVl4C-__TncDblDutSy82CjNuo",
+  cloud_name: "drwwvb3kt",
+  api_key: "269268292399213",
+  api_secret: "qUu9PtlZbbCAzefEe3s_Rl7euMw",
 });
 export default cloudinary.v2;
