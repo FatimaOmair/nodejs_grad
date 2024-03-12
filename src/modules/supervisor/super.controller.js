8796919acaa7ab466e7dd7805a620a6711ce9a5a
Tasneem <tasneemgazal5@gmail.com>
2024-03-12 08:01:57 +0200

    add get my section function

--- src/modules/supervisor/super.controller.js
+++ src/modules/supervisor/super.controller.js
@@ -4,30 +4,46 @@ import { getUser } from "../../services/getId.js";
 import { getUsers } from "../admin/admin.controller.js";
 
 export const reject = async (req, res, next) => {
-   
-    try {
-     const request = await requestModel.findByIdAndUpdate(req.body.requestId,{state:"rejected"},{new:true});
-     await sectionModel.findByIdAndUpdate(req.body.sectionId,{visible:true});
-     return res.status(201).json(request);
-    } catch (err) {
-      next(new Error(err.message, { cause: 500 }));
-    }
-  };
+  try {
+    const request = await requestModel.findByIdAndUpdate(
+      req.body.requestId,
+      { state: "rejected" },
+      { new: true }
+    );
+    await sectionModel.findByIdAndUpdate(req.body.sectionId, { visible: true });
+    return res.status(201).json(request);
+  } catch (err) {
+    next(new Error(err.message, { cause: 500 }));
+  }
+};
 
-  export const confirm = async (req, res, next) => {
-   
-    try {
-     const {sectionId,requestId,students}=req.body
-     const arr=[]
-     for(let i=0; i<students.length; i++) {
-        const user=await getUser(students[i])
-        arr.push(user._id)
-     }
-
-     const request = await requestModel.findByIdAndUpdate(req.body.requestId,{state:"accept"},{new:true});
-     await sectionModel.findByIdAndUpdate(req.body.sectionId,{students:arr});
-     return res.status(201).json(request);
-    } catch (err) {
-      next(new Error(err.message, { cause: 500 }));
+export const confirm = async (req, res, next) => {
+  try {
+    const { sectionId, requestId, students } = req.body;
+    const arr = [];
+    for (let i = 0; i < students.length; i++) {
+      const user = await getUser(students[i]);
+      arr.push(user._id);
     }
-  };
+
+    const request = await requestModel.findByIdAndUpdate(
+      req.body.requestId,
+      { state: "accept" },
+      { new: true }
+    );
+    await sectionModel.findByIdAndUpdate(req.body.sectionId, { students: arr });
+    return res.status(201).json(request);
+  } catch (err) {
+    next(new Error(err.message, { cause: 500 }));
+  }
+};
+ 
+export const getMySections = async (req, res, next) => {
+  try {
+    const {id} = req.params;
+    const section = await sectionModel.find({userId: id});
+    return res.status(201).json(section);
+  } catch (err) {
+    next(new Error(err.message, { cause: 500 }));
+  }
+};
--- src/modules/supervisor/super.router.js
+++ src/modules/supervisor/super.router.js
@@ -1,7 +1,7 @@
 import { Router } from "express";
-import { confirm, reject } from "./super.controller.js";
+import { confirm, getMySections, reject } from "./super.controller.js";
 const router = Router();
 router.post('/reject',reject);
 router.post('/confirm',confirm);
-
+router.get('/getSections/:id',getMySections);
 export default router;
