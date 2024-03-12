50e6d7f70afb2e34ef7559b3371b50cce54d99b8
Tasneem <tasneemgazal5@gmail.com>
2024-03-12 09:29:39 +0200

    add get and delete end points

--- src/modules/headOfdepartment/head.router.js
+++ src/modules/headOfdepartment/head.router.js
@@ -1,5 +1,9 @@
 import { Router } from "express";
-import { createSection } from "./head.controller.js";
+import { createSection, deleteSection, getHeadSections } from "./head.controller.js";
+import { auth } from "../../middleWare/auth.js";
+import { role } from "../../services/role.js";
 const router = Router();
 router.post('/addSection',createSection)
+router.get('/getHeadSections',auth([role.headOfDepartment]),getHeadSections)
+router.delete('/deleteSection/:id',auth([role.headOfDepartment]),deleteSection)
 export default router;
