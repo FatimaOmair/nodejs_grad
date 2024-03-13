50d3521dfeef105c7e8cdee38af53153181aa70c
FatimaOmair <fatimaemer2002@gmail.com>
2024-03-13 12:41:36 +0200

    take depId from token for create section

--- src/modules/headOfdepartment/head.controller.js
+++ src/modules/headOfdepartment/head.controller.js
@@ -2,15 +2,13 @@ import { sectionModel } from "../../../DB/model/section.model.js";
 
 export const createSection = async (req, res, next) => {
   try {
-    const { num, depId, userId } = req.body;
-    const section = await sectionModel.create({ num, depId, userId });
-    return res.status(201).json(section);
+    const { num, userId } = req.body;
+    const section = await sectionModel.create({ num, depId:req.depId, userId });
+    return res.status(200).json(section);
   } catch (err) {
     next(new Error(err.message, { cause: 500 }));
   }
 };
-
-
 
 export const getHeadSections = async (req, res, next) => {
   try {
@@ -40,4 +38,7 @@ export const deleteSection = async (req, res, next) => {
     next(new Error(err.message, { cause: 500 }));
   }
 };
+
+
+
 
