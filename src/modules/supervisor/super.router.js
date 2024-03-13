e1d53977b4891b6821aacdc0bc6c50b6f18cade1
FatimaOmair <fatimaemer2002@gmail.com>
2024-03-13 12:42:27 +0200

    add assign task route

--- src/modules/supervisor/super.router.js
+++ src/modules/supervisor/super.router.js
@@ -1,9 +1,12 @@
 import { Router } from "express";
-import { confirm, getMySections, reject } from "./super.controller.js";
+import { assignTask, confirm, getMySections, reject } from "./super.controller.js";
 import { role } from "../../services/role.js";
 import { auth } from "../../middleWare/auth.js";
+import { HME, multerValidation, myMulter } from "../../services/multer.js";
 const router = Router();
 router.post('/reject',reject);
 router.post('/confirm',confirm);
 router.get('/getSections',auth([role.supervisor]),getMySections);
+router.post('/assignTask', myMulter(multerValidation.pdf).single("task"),
+HME,assignTask);
 export default router;
