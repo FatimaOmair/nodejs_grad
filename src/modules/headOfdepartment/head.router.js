387d6196e5f901d48cc212e1b16c29a315b8f93a
Tasneem <tasneemgazal5@gmail.com>
2024-03-14 21:18:42 +0200

    add viewProfile,updateProfile, updateHeadSections end points

--- src/modules/headOfdepartment/head.router.js
+++ src/modules/headOfdepartment/head.router.js
@@ -1,9 +1,13 @@
 import { Router } from "express";
-import { createSection, deleteSection, getHeadSections } from "./head.controller.js";
+import { createSection, deleteSection, getHeadSections, updateHeadSections } from "./head.controller.js";
 import { auth } from "../../middleWare/auth.js";
 import { role } from "../../services/role.js";
+import { editProfile, viewProfile } from "../../services/profile.js";
 const router = Router();
 router.post('/addSection',createSection)
 router.get('/getHeadSections',auth([role.headOfDepartment]),getHeadSections)
 router.delete('/deleteSection/:id',auth([role.headOfDepartment]),deleteSection)
+router.get('/profile',auth([role.headOfDepartment]), viewProfile);
+router.patch('/editProfile', auth([role.headOfDepartment]), editProfile);
+router.patch('/updateSection', auth([role.headOfDepartment]), updateHeadSections);
 export default router;
