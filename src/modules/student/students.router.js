683973646fc4d960a90ed098e8d6fc1e348eb8fc
FatimaOmair <fatimaemer2002@gmail.com>
2024-03-14 09:45:22 +0200

    add route for view profile

--- src/modules/student/students.router.js
+++ src/modules/student/students.router.js
@@ -1,5 +1,7 @@
 import { Router } from "express";
-import { bookSection } from "./student.controller.js";
+import { bookSection, viewProfile } from "./student.controller.js";
+
 const router = Router();
 router.post('/bookSection',bookSection);
+router.get('/profile', viewProfile);
 export default router;
