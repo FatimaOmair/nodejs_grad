f9bd3a617bf6067ba4694e29186f5f0293f9a0ef
FatimaOmair <fatimaemer2002@gmail.com>
2024-03-14 09:46:31 +0200

    add route for feedback , view and edit profile

--- src/modules/supervisor/super.router.js
+++ src/modules/supervisor/super.router.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { assignTask, confirm, getMySections, reject } from "./super.controller.js";
+import { assignTask, confirm, editProfile, getMySections, giveFeedback, reject, viewProfile } from "./super.controller.js";
 import { role } from "../../services/role.js";
 import { auth } from "../../middleWare/auth.js";
 import { HME, multerValidation, myMulter } from "../../services/multer.js";
@@ -9,4 +9,7 @@ router.post('/confirm',confirm);
 router.get('/getSections',auth([role.supervisor]),getMySections);
 router.post('/assignTask', myMulter(multerValidation.pdf).single("task"),
 HME,assignTask);
+router.post('/feedback/:id', giveFeedback);
+router.get('/profile',auth([role.supervisor]), viewProfile);
+router.put('/editProfile', auth([role.supervisor]), editProfile);
 export default router;
