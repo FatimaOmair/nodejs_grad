f6beffeea5a54e880ec62d771086d877cc93a0af
Tasneem <tasneemgazal5@gmail.com>
2024-03-14 21:20:49 +0200

    add viewProfile, editProfile,submit Task

--- src/modules/student/students.router.js
+++ src/modules/student/students.router.js
@@ -1,7 +1,14 @@
 import { Router } from "express";
-import { bookSection, viewProfile } from "./student.controller.js";
+import { bookSection, submitTask} from "./student.controller.js";
+import { auth } from "../../middleWare/auth.js";
+import { role } from "../../services/role.js";
+import { editProfile, viewProfile } from "../../services/profile.js";
+import { HME, multerValidation, myMulter } from "../../services/multer.js";
 
 const router = Router();
-router.post('/bookSection',bookSection);
-router.get('/profile', viewProfile);
+router.post('/bookSection',auth([role.student]),bookSection);
+router.get('/profile',auth([role.student]), viewProfile);
+router.patch('/editProfile',auth([role.student]), editProfile);
+router.post('/submitTask', myMulter(multerValidation.pdf).single("task"),
+HME,submitTask);
 export default router;
