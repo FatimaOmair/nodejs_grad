4e0c3120d188dc5af07fa09a602a3ab372a68387
FatimaOmair <fatimaemer2002@gmail.com>
2024-03-16 10:34:18 +0200

    add router for updateHeadSections end point

--- src/modules/headOfdepartment/head.router.js
+++ src/modules/headOfdepartment/head.router.js
@@ -1,13 +1,14 @@
 import { Router } from "express";
-import { createSection, deleteSection, getHeadSections, updateHeadSections } from "./head.controller.js";
+import { createSection, deleteSection, deleteStudentsFromSection, getHeadSections, updateHeadSections } from "./head.controller.js";
 import { auth } from "../../middleWare/auth.js";
 import { role } from "../../services/role.js";
 import { editProfile, viewProfile } from "../../services/profile.js";
 const router = Router();
-router.post('/addSection',createSection)
+router.post('/addSection',auth([role.headOfDepartment]),createSection)
 router.get('/getHeadSections',auth([role.headOfDepartment]),getHeadSections)
 router.delete('/deleteSection/:id',auth([role.headOfDepartment]),deleteSection)
 router.get('/profile',auth([role.headOfDepartment]), viewProfile);
 router.patch('/editProfile', auth([role.headOfDepartment]), editProfile);
-router.patch('/updateSection', auth([role.headOfDepartment]), updateHeadSections);
+router.patch('/updateSection/:sectionId', auth([role.headOfDepartment]), updateHeadSections);
+router.delete('/deleteStudents/:sectionId', deleteStudentsFromSection);
 export default router;
