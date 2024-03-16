e48f24c5014b46ae3ef628ff989d045c63d27f44
Tasneem <tasneemgazal5@gmail.com>
2024-03-16 08:32:36 +0200

    make middle ware auth shared in order to apply DRY

--- src/modules/headOfdepartment/head.router.js
+++ src/modules/headOfdepartment/head.router.js
@@ -4,10 +4,11 @@ import { auth } from "../../middleWare/auth.js";
 import { role } from "../../services/role.js";
 import { editProfile, viewProfile } from "../../services/profile.js";
 const router = Router();
+router.use(auth([role.headOfDepartment]));
 router.post('/addSection',createSection)
-router.get('/getHeadSections',auth([role.headOfDepartment]),getHeadSections)
-router.delete('/deleteSection/:id',auth([role.headOfDepartment]),deleteSection)
-router.get('/profile',auth([role.headOfDepartment]), viewProfile);
-router.patch('/editProfile', auth([role.headOfDepartment]), editProfile);
-router.patch('/updateSection', auth([role.headOfDepartment]), updateHeadSections);
+router.get('/getHeadSections',getHeadSections)
+router.delete('/deleteSection/:id',deleteSection)
+router.get('/profile', viewProfile);
+router.patch('/editProfile', editProfile);
+router.patch('/updateSection', updateHeadSections);
 export default router;
