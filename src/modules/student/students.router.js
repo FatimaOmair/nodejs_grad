bd2d6a0605061eacf2c565ca2ae9042a1075af09
Tasneem <tasneemgazal5@gmail.com>
2024-03-25 21:58:47 +0200

    add get student section router

--- src/modules/student/students.router.js
+++ src/modules/student/students.router.js
@@ -1,14 +1,14 @@
 import { Router } from "express";
-import { bookSection, submitTask} from "./student.controller.js";
+import { bookSection, getStudentSection, submitTask} from "./student.controller.js";
 import { auth } from "../../middleWare/auth.js";
 import { role } from "../../services/role.js";
 import { editProfile, viewProfile } from "../../services/profile.js";
 import { HME, multerValidation, myMulter } from "../../services/multer.js";
-
 const router = Router();
 router.post('/bookSection',auth([role.student]),bookSection);
 router.get('/profile',auth([role.student]), viewProfile);
 router.patch('/editProfile',auth([role.student]), editProfile);
 router.post('/submitTask/:sectionId/:taskId', myMulter(multerValidation.pdf).single("task"),
 HME,submitTask);
+router.get("/section",auth([role.student]), getStudentSection);
 export default router;
