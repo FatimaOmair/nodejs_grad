ed04af1ffb1af19ca2946c8d71fe263cc8dc2fd5
Tasneem <tasneemgazal5@gmail.com>
2024-04-03 23:19:13 +0300

    fix booking issue to prevent student be in more than one section

--- src/modules/student/student.controller.js
+++ src/modules/student/student.controller.js
@@ -2,18 +2,39 @@ import { requestModel } from "../../../DB/model/request.model.js";
 import { sectionModel } from "../../../DB/model/section.model.js";
 import { submitModel } from "../../../DB/model/submit.model.js";
 import { taskModel } from "../../../DB/model/task.model.js";
+import { getUser } from "../../services/getId.js";
 import { uploadFile } from "../../services/uploadFile.js";
 
 export const bookSection = async (req, res, next) => {
-    try {
-     const {text,studentId,sectionId}=req.body;
-     const request = await requestModel.create({text,studentId,sectionId});
-     await sectionModel.findByIdAndUpdate(sectionId,{visible:false});
-     return res.status(201).json({message:"success",request});
-    } catch (err) {
+  try {
+      const { students, studentId, sectionId } = req.body;
+      const studentObjectIds = [];
+      for (let i = 0; i < students.length; i++) {
+          const student = await getUser(students[i]);
+          if (student) {
+              studentObjectIds.push(student._id);
+          } else {
+              return res.status(400).json({ message: `Student with university number ${students[i]} not found` });
+          }
+      }
+      // Check if any of the students are already in another section
+      for (let i = 0; i < studentObjectIds.length; i++) {
+          const studentExists = await sectionModel.findOne({
+              students: { $in: [studentObjectIds[i]] },
+              _id: { $ne: sectionId } 
+          });
+          if (studentExists) {
+              return res.status(400).json({ message: `Student with university number ${students[i]} is already in another section` });
+          }
+      }
+
+      const request = await requestModel.create({ students: studentObjectIds, studentId, sectionId });
+      await sectionModel.findByIdAndUpdate(sectionId, { visible: false });
+      return res.status(201).json({ message: "success", request });
+  } catch (err) {
       next(new Error(err.message, { cause: 500 }));
-    }
-  };
+  }
+};
 
   export const getStudentSection = async (req, res, next) => {
     try {
