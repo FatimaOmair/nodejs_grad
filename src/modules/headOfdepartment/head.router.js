9151eeee6ed6087ae3666d1a20bb549619226d48
Tasneem <tasneemgazal5@gmail.com>
2024-04-07 15:53:11 +0300

    remove duplicate routes

--- src/modules/headOfdepartment/head.router.js
+++ src/modules/headOfdepartment/head.router.js
@@ -5,12 +5,6 @@ import { role } from "../../services/role.js";
 import { editProfile, viewProfile } from "../../services/profile.js";
 const router = Router();
 router.get('/getHeadSections',auth([role.headOfDepartment,role.student,role.supervisor]),getHeadSections)
-router.post('/addSection',auth([role.headOfDepartment]),createSection)
-router.get('/getHeadSections',auth([role.headOfDepartment]),getHeadSections)
-router.delete('/deleteSection/:id',auth([role.headOfDepartment]),deleteSection)
-router.get('/profile',auth([role.headOfDepartment]), viewProfile);
-router.patch('/editProfile', auth([role.headOfDepartment]), editProfile);
-router.patch('/updateSection/:sectionId', auth([role.headOfDepartment]), updateHeadSections);
 router.use(auth([role.headOfDepartment]));
 router.post('/addSection',createSection)
 router.delete('/deleteSection/:id',deleteSection)
