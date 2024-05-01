21b80bed260fb372dfa880808466decd28948fc9
Tasneem <tasneemgazal5@gmail.com>
2024-05-01 14:22:53 +0300

    add chat router

--- src/modules/index.router.js
+++ src/modules/index.router.js
@@ -3,4 +3,5 @@ import adminRouter from './admin/admin.router.js'
 import headRouter from './headOfdepartment/head.router.js'
 import studentRouter from './student/students.router.js'
 import supervisorRouter from './supervisor/super.router.js'
-export { authRouter, adminRouter,headRouter,studentRouter,supervisorRouter }
+import chatRouter from './chat/chat.router.js'
+export { authRouter, adminRouter,headRouter,studentRouter,supervisorRouter ,chatRouter } 
