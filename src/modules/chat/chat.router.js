a9a4ce985a7fddbac4e51f13eb79b35e16c38ec8
Tasneem <tasneemgazal5@gmail.com>
2024-05-01 14:24:59 +0300

    create chat router file

---
+++ src/modules/chat/chat.router.js
@@ -0,0 +1,10 @@
+import { Router } from "express";
+import { fetchChat,getMessages,sendMessage } from "./chat.controller.js";
+import { auth } from "../../middleWare/auth.js";
+import { role } from "../../services/role.js";
+const router = Router();
+router.post("/sendmessage/:chatId", auth([role.supervisor,role.student]), sendMessage);
+router.get('/fetchChat',auth([role.supervisor]),fetchChat);
+router.get("/getmessages/:chatId", auth([role.supervisor]), getMessages);
+
+export default router;
