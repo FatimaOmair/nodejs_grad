1d6a9d0bc6e35a180308798068e902ac31a8459c
Tasneem <tasneemgazal5@gmail.com>
2024-05-11 11:00:51 +0300

    change message model attributes

--- DB/model/message.model.js
+++ DB/model/message.model.js
@@ -1,24 +1,21 @@
-import { Schema, Types, model } from "mongoose";
-import mongoosePaginate from "mongoose-paginate-v2";
-
+import { Schema, Types,model } from "mongoose";
 const messageSchema = new Schema(
   {
-    senderId: {
+    senderSuper: {
       type: Types.ObjectId,
+      ref: "user",
     },
-    senderRole: String,
+    senderStd:{
+      type: Types.ObjectId,
+      ref: "student"
+    },
     content: { type: String, trim: true },
-    sectionId: { type: Types.ObjectId, ref: "section" },
-    isImage: { type: Boolean, default: false } 
+    chat: { type: Types.ObjectId, ref: "chat" },
   },
-  {
-    timestamps: true,
+  { timestamps: true,
     toJSON: { virtuals: true },
-    toObject: { virtuals: true }
-  }
+    toObject: { virtuals: true }}
 );
-messageSchema.plugin(mongoosePaginate);
-
 
 export const messageModel = model("message", messageSchema);
 
