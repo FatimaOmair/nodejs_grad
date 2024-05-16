eda3e8aa191b085fc12c48a0ff083e90b7a9bba3
Tasneem <tasneemgazal5@gmail.com>
2024-05-16 18:22:46 +0300

    add fetchSectionChat

--- src/modules/chat/chat.router.js
+++ src/modules/chat/chat.router.js
@@ -1,10 +1,12 @@
 import { Router } from "express";
-import { fetchChat,getMessages,sendMessage } from "./chat.controller.js";
+import { fetchChat,getMessages,getSectionChatMapping,sendMessage } from "./chat.controller.js";
 import { auth } from "../../middleWare/auth.js";
 import { role } from "../../services/role.js";
 const router = Router();
 router.post("/sendmessage/:chatId", auth([role.supervisor,role.student]), sendMessage);
 router.get('/fetchChat',auth([role.supervisor,role.student]),fetchChat);
+router.get('/fetchSectionChat',auth([role.supervisor]),getSectionChatMapping);
 router.get("/getmessages/:chatId", auth([role.supervisor,role.student]), getMessages);
+//router.get("/getothermessages/:chatId", auth([role.supervisor,role.student]), getOtherMessages);
 
 export default router;
