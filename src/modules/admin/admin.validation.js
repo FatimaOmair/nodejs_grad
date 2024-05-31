9b1d9e9f815d3cc675875a60d903ec000bbd161a
Tasneem <tasneemgazal5@gmail.com>
2024-05-31 18:15:56 +0300

    handle validation issues

--- src/modules/admin/admin.validation.js
+++ src/modules/admin/admin.validation.js
@@ -4,23 +4,20 @@ Joi.objectId = objectId(Joi);
 
 export const createDepValidation = {
     body: Joi.object().required().keys({
-        name: Joi.string().max(50).required().messages({
+        name: Joi.string().min(2).max(50).required().messages({
           "any.required": "Send a name for the department, please",
           "string.empty": "name is required",
         }),
-       
       }),
   };
-
-  export const createProjectValidation = {
+export const createProjectValidation = {
     body: Joi.object().required().keys({
         name: Joi.string().max(50).required().messages({
           "any.required": "Send a name for the user, please",
           "string.empty": "name is required",
         }),
-        group: Joi.array().items(Joi.string()).required(),
-        supervisorname: Joi.string().max(50).required(),
+        group: Joi.required(),
+        supervisorName: Joi.string().min(3).max(20).required(),
         depId:Joi.objectId().required()
-       
       }),
   };
--- src/modules/auth/auth.validation.js
+++ src/modules/auth/auth.validation.js
@@ -3,33 +3,30 @@ import objectId from 'joi-objectid';
 Joi.objectId = objectId(Joi);
 export const signupValidation = {
     body: Joi.object().required().keys({
-        name: Joi.string().min(3).max(10).required().messages({
+        name: Joi.string().min(3).max(20).required().messages({
           "any.required": "Send a name for the user, please",
           "string.empty": "name is required",
         }),
         email: Joi.string().email().required(),
         password: Joi.string().min(5).max(20).required(),
-        phoneNumber: Joi.number().required(),
-        role: Joi.string().required(),
-        officeHours:Joi.string(),
-        depId:Joi.objectId()
-       
-        
+        phoneNumber: Joi.number().min(10).max(10).required(),
+        role: Joi.alternatives().try(Joi.string(), Joi.array().items(Joi.string())).required(),
+        officeHours:Joi.string().required(),
+        depId:Joi.objectId().required(),
       }),
   };
 
   export const studentValidation = {
     body: Joi.object().required().keys({
-        name: Joi.string().min(3).max(10).required().messages({
+        name: Joi.string().min(3).max(20).required().messages({
           "any.required": "Send a name for the user, please",
           "string.empty": "name is required",
         }),
         email: Joi.string().email().required(),
         password: Joi.string().min(5).max(20).required(),
-        phoneNumber: Joi.number().required(),
+        phoneNumber: Joi.number().min(10).max(10).required(),
         academicYear: Joi.number().required(),
-        depId:Joi.objectId().required()
-       
+        depId:Joi.objectId().required(),
       }),
   };
 
