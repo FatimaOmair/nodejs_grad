777b71344eb2194ba24d0c02169cb81583a4126d
FatimaOmair <fatimaemer2002@gmail.com>
2024-06-22 10:16:19 +0300

    edit verify code function

--- src/services/verifyCode.js
+++ src/services/verifyCode.js
@@ -1,22 +1,21 @@
 import nodemailer from "nodemailer";
 
- const verifyCode = async (dest,code,subject) => {
-    let transporter = nodemailer.createTransport({
-      service: "gmail",
-      auth: {
-        user: process.env.EMAIL, 
-        pass: process.env.PASSWORD,
-      },
-      tls: {
-        rejectUnauthorized: false
-      }
-    });
+const verifyCode = async (dest, subject, code) => {
+  let transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: process.env.EMAIL,
+      pass: process.env.PASSWORD,
+    },
+    tls: {
+      rejectUnauthorized: false,
+    },
+  });
 
-    let htmlTemplate =`
+  let htmlTemplate = `
     <html>
     <head>
       <style>
-        /* CSS styles for your email template */
         body {
           font-family: Arial, sans-serif;
           background-color: #573e66;
@@ -49,13 +48,13 @@ import nodemailer from "nodemailer";
           border-bottom-right-radius: 10px;
         }
         .graduation-img {
-          width: 150px; /* Adjust the size as needed */
-          float: right; /* Align the image to the right */
-          margin-left: 20px; /* Add some spacing between the image and paragraphs */
-          margin-top: 20px; /* Add some spacing between the image and paragraphs */
+          width: 150px;
+          float: right;
+          margin-left: 20px;
+          margin-top: 20px;
         }
         .text-left {
-          text-align: left; /* Align paragraphs to the left */
+          text-align: left;
         }
         @keyframes slideIn {
           from {
@@ -74,31 +73,28 @@ import nodemailer from "nodemailer";
     </head>
     <body>
       <div class="container">
-      <div class="header">
-      <h1>Password Reset Verification</h1>
-    </div>
+        <div class="header">
+          <h1>Password Reset Verification</h1>
+        </div>
         <div class="content animated">
           <img src="https://media.gettyimages.com/id/1423774582/vector/graduation-send-off-green-suit-building-background.jpg?s=612x612&w=gi&k=20&c=WrcCvbY6w13ofbotWs1eLoEzqWEcVDyLG4FTmQGrEzU=" alt="Graduation Icon" class="graduation-img">
-          
           <p class="text-left">Hello,</p>
-          <p class="text-center">You've requested to reset your password. Please use this code to verify:</p>
-          <p class="text-left"><strong>${code}.</strong></p>
-         
+          <p class="text-left">You've requested to reset your password. Please use this code to verify:</p>
+          <p class="text-left"><strong>${code}</strong></p>
         </div>
         <div class="footer">
-          <p> [Palestine Technical University Khadoorie]</p>
-       
+          <p>Palestine Technical University Khadoorie</p>
         </div>
       </div>
     </body>
     </html>`;
 
-    let info = await transporter.sendMail({
-      from:process.env.EMAIL, 
-      to:dest  ,
-      subject:"GMS verification email",
-      html: htmlTemplate, 
-    });
-  };
+  let info = await transporter.sendMail({
+    from: process.env.EMAIL,
+    to: dest,
+    subject: subject,
+    html: htmlTemplate,
+  });
+};
 
-  export default verifyCode;
+export default verifyCode;
